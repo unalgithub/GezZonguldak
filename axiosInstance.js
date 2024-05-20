7dcc7feeb92dc9c26f698d42f734497e67ce32e8
Göktuğ <trabzongok@gmail.com>
2024-05-20 15:56:17 +0300

    Co-authored-by: BERAT BURAK KAYA <beratkaya61@users.noreply.github.com>

--- axiosInstance.js
+++ axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://192.168.1.106:3000/api",
+  baseURL: "http://192.168.1.103:3000/api",
+  // baseURL: "http://192.168.1.106:3000/api",
+
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json; charset=utf-8",
