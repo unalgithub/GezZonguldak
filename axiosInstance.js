21b65e82746b447bf4f1d212cee4b7b1bfd289cb
unalgithub <unalgithub@gmail.com>
2024-05-22 14:26:27 +0300

    harita

--- axiosInstance.js
+++ axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://192.168.1.103:3000/api",
+  baseURL: "http://192.168.0.12:3000/api",
+  //baseURL: "http://192.168.1.102:3000/api",
   // baseURL: "http://192.168.1.106:3000/api",
 
   headers: {
--- screens/Tabs/Map.js
+++ screens/Tabs/Map.js
@@ -1,402 +1,147 @@
-import { View, StyleSheet, Dimensions } from "react-native";
+import { View, StyleSheet, Dimensions,Image } from "react-native";
 import React, { useState, useRef, useEffect } from "react";
-import MapView, { Explain } from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
 
 const mapStyle = [
   {
-    featureType: "all",
-    elementType: "geometry",
-    stylers: [
-      {
-        visibility: "on",
-      },
-    ],
+      "featureType": "landscape.man_made",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "color": "#f7f1df"
+          }
+      ]
   },
   {
-    featureType: "all",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
+      "featureType": "landscape.natural",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "color": "#d0e3b4"
+          }
+      ]
   },
   {
-    featureType: "administrative",
-    elementType: "all",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
+      "featureType": "landscape.natural.terrain",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
   },
   {
-    featureType: "administrative",
-    elementType: "geometry",
-    stylers: [
-      {
-        visibility: "off",
-      },
-      {
-        weight: "0.50",
-      },
-    ],
+      "featureType": "poi",
+      "elementType": "labels",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
   },
   {
-    featureType: "administrative",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
+      "featureType": "poi.business",
+      "elementType": "all",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
   },
   {
-    featureType: "administrative.country",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "on",
-      },
-    ],
+      "featureType": "poi.medical",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "color": "#fbd3da"
+          }
+      ]
   },
   {
-    featureType: "administrative.locality",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "simplified",
-      },
-    ],
+      "featureType": "poi.park",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "color": "#bde6ab"
+          }
+      ]
   },
   {
-    featureType: "administrative.neighborhood",
-    elementType: "all",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
+      "featureType": "road",
+      "elementType": "geometry.stroke",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
   },
   {
-    featureType: "landscape.man_made",
-    elementType: "all",
-    stylers: [
-      {
-        visibility: "simplified",
-      },
-    ],
+      "featureType": "road",
+      "elementType": "labels",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
   },
   {
-    featureType: "landscape.man_made",
-    elementType: "geometry.fill",
-    stylers: [
-      {
-        color: "#ece2d9",
-      },
-    ],
+      "featureType": "road.highway",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#ffe15f"
+          }
+      ]
   },
   {
-    featureType: "landscape.man_made",
-    elementType: "geometry.stroke",
-    stylers: [
-      {
-        visibility: "simplified",
-      },
-    ],
+      "featureType": "road.highway",
+      "elementType": "geometry.stroke",
+      "stylers": [
+          {
+              "color": "#efd151"
+          }
+      ]
   },
   {
-    featureType: "landscape.man_made",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "on",
-      },
-    ],
+      "featureType": "road.arterial",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#ffffff"
+          }
+      ]
   },
   {
-    featureType: "landscape.man_made",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
+      "featureType": "road.local",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "black"
+          }
+      ]
   },
   {
-    featureType: "landscape.man_made",
-    elementType: "labels.text.stroke",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
+      "featureType": "transit.station.airport",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#cfb2db"
+          }
+      ]
   },
   {
-    featureType: "landscape.natural",
-    elementType: "geometry.fill",
-    stylers: [
-      {
-        visibility: "on",
-      },
-      {
-        color: "#b8cb93",
-      },
-    ],
-  },
-  {
-    featureType: "landscape.natural.landcover",
-    elementType: "geometry",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "landscape.natural.terrain",
-    elementType: "geometry",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "poi",
-    elementType: "all",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "poi",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "simplified",
-      },
-    ],
-  },
-  {
-    featureType: "poi.attraction",
-    elementType: "all",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "poi.attraction",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "poi.business",
-    elementType: "all",
-    stylers: [
-      {
-        visibility: "simplified",
-      },
-    ],
-  },
-  {
-    featureType: "poi.business",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "poi.government",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "poi.medical",
-    elementType: "all",
-    stylers: [
-      {
-        visibility: "on",
-      },
-    ],
-  },
-  {
-    featureType: "poi.medical",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "poi.park",
-    elementType: "all",
-    stylers: [
-      {
-        visibility: "on",
-      },
-    ],
-  },
-  {
-    featureType: "poi.park",
-    elementType: "geometry.fill",
-    stylers: [
-      {
-        color: "#ccdca1",
-      },
-    ],
-  },
-  {
-    featureType: "poi.park",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "poi.place_of_worship",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "poi.school",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "poi.sports_complex",
-    elementType: "all",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "poi.sports_complex",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "road",
-    elementType: "geometry.fill",
-    stylers: [
-      {
-        hue: "#ff0000",
-      },
-      {
-        saturation: -100,
-      },
-      {
-        lightness: 99,
-      },
-    ],
-  },
-  {
-    featureType: "road",
-    elementType: "geometry.stroke",
-    stylers: [
-      {
-        color: "#808080",
-      },
-      {
-        lightness: 54,
-      },
-    ],
-  },
-  {
-    featureType: "road",
-    elementType: "labels.text.fill",
-    stylers: [
-      {
-        color: "#767676",
-      },
-    ],
-  },
-  {
-    featureType: "road",
-    elementType: "labels.text.stroke",
-    stylers: [
-      {
-        color: "#ffffff",
-      },
-    ],
-  },
-  {
-    featureType: "road.highway",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "road.arterial",
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {
-    featureType: "water",
-    elementType: "all",
-    stylers: [
-      {
-        saturation: 43,
-      },
-      {
-        lightness: -11,
-      },
-      {
-        hue: "#0088ff",
-      },
-    ],
-  },
-  {
-    featureType: "water",
-    elementType: "geometry",
-    stylers: [
-      {
-        color: "#153474",
-      },
-    ],
-  },
-];
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "color": "#a2daf2"
+          }
+      ]
+  }
+]
 
 const MapTab = ({ route }) => {
   const mapRef = useRef(null);
@@ -407,17 +152,51 @@ const MapTab = ({ route }) => {
     longitudeDelta: 0.0421,
   };
 
+  const markerCoordinates = [
+    { latitude: 41.455712449162085, longitude: 31.786199842580277, title: "Zonguldak Limanı",markerIcon: require('../../assets/liman.png'), },
+    { latitude: 41.4475449782086, longitude: 31.8145918249371, title: "Zonguldak maden müzesi" ,markerIcon: require('../../assets/müze.png'),},
+    { latitude: 41.44076328395787, longitude: 31.833154139771036 , title: "Gökgöl mağarası",markerIcon: require('../../assets/mağara.png'), },
+    { latitude: 41.561906743741226, longitude: 32.024309313402924 , title: "Filyos Çarşısı",markerIcon: require('../../assets/çarşı.png'), },
+    { latitude: 41.573871476167, longitude: 32.03193481279399, title: "Tios antik kenti" ,markerIcon: require('../../assets/antik.png'), },
+    { latitude: 41.45266677764984, longitude: 31.98818438970079 , title: "Çakırköy mağarası",markerIcon: require('../../assets/mağara.png'), },
+    { latitude: 41.4129454559085, longitude: 31.793449747890175 , title: "Ulutan barajı",markerIcon: require('../../assets/baraj.png'), },
+    { latitude: 41.05379472948139, longitude: 31.654335795574134 , title: "Gümeli porsuk ağacı" ,markerIcon: require('../../assets/ağaç.png'),},
+    { latitude: 41.05227232173551, longitude: 31.678859394631534 , title: "Bölüklü yaylası",markerIcon: require('../../assets/yayla.png'), },
+    { latitude: 41.317708629297826, longitude: 31.451722071694032 , title: "Cehennemağzı mağaraları" ,markerIcon: require('../../assets/mağara.png'),},
+    { latitude:41.46428421566327, longitude: 31.787217982614084 , title: "Deniz feneri" ,markerIcon: require('../../assets/fener.png'),},
+    { latitude: 41.47213804942162, longitude: 31.80301024315775 , title: "Kapuz plajı" ,markerIcon: require('../../assets/plaj.png'),},
+    { latitude:41.39646333586999, longitude: 31.84593684165429 , title: "Zonguldak kent ormanı",markerIcon: require('../../assets/orman.png'), },
+    { latitude: 41.225547043113416, longitude: 31.964600830992776, title: "Bastoncular çarşısı" ,markerIcon: require('../../assets/çarşı.png'), },
+    { latitude: 41.40840258466706, longitude:31.68452254157608 , title: "Ilıksu plajı" ,markerIcon: require('../../assets/plaj.png'),},
+    { latitude: 41.465387392013234, longitude: 31.78834865377808 , title: "Varagel tüneli",markerIcon: require('../../assets/tunel.png'), },
+    { latitude: 41.42464796014042, longitude:31.818361901962916 , title: "Harmankaya şelalesi",markerIcon: require('../../assets/şelale.png'), },
+    
+    
+  ];
+  
+
   return (
     <View style={styles.container}>
       <MapView
         provider="google"
         showsUserLocation={true}
         showsMyLocationButton={true}
-        customMapStyle={mapStyle}
         ref={mapRef}
+        customMapStyle={mapStyle}
         style={styles.map}
         initialRegion={initialRegion}
-      ></MapView>
+      >
+        {markerCoordinates.map((marker, index) => (
+           <Marker
+           key={index}
+           coordinate={marker}
+           title={marker.title}
+         >
+           <Image source={marker.markerIcon} style={{ width: 30, height: 30 }} />
+         </Marker>
+          
+        ))}
+      </MapView>
     </View>
   );
 };
